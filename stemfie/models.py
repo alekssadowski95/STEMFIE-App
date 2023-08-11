ad2b74291455e076892853c55a96f4e17b5f0695
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2023-08-11 12:05:17 +0200

    update

--- stemfie/models.py
+++ stemfie/models.py
@@ -5,9 +5,12 @@ class Product(db.Model):
     # required
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(db.String(32), unique=True, nullable=False)
-    name = db.Column(db.String(100), nullable=False)
     # default
+    category = db.Column(db.String(100), nullable=False, default='Uncategorized')
+    name = db.Column(db.String(100), nullable=False, default='Unnamed')
+    relevance = db.Column(db.Integer, nullable=False, default=50)
     view_count = db.Column(db.Integer, nullable=False, default=0)
     is_active = db.Column(db.Boolean, nullable=False, default=True)  
-    preview_stl_filename = db.Column(db.String(100), nullable=False, default='')
-    download_stl_filename = db.Column(db.String(100), nullable=False, default='')
+    preview_img_filename = db.Column(db.String(100), nullable=False, default='default-blocks-preview-img.jpg')
+    preview_3d_filename = db.Column(db.String(100), nullable=False, default='default-blocks-preview-3d.stl')
+    download_3d_filename = db.Column(db.String(100), nullable=False, default='default-blocks-download-3d.stl')
